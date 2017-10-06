334ab67ed70ef97c6dcfce781eb4ab73eb6641cb
Julio Velarde <cevegaju@hotmail.com>
2017-10-05 21:57:00 -0500

    Creación de Endpoints Creación de validador y uso en controlador.

--- Cibertec/Cibertec.WebApi/Startup.cs
+++ Cibertec/Cibertec.WebApi/Startup.cs
@@ -4,6 +4,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Cibertec.Repositories.Dapper.Northwind;
 using Cibertec.UnitOfWork;
+using FluentValidation.AspNetCore;
+using Cibertec.Models;
+using Cibertec.WebApi.Validators;
+using FluentValidation;
 
 namespace Cibertec.WebApi
 {
@@ -16,14 +20,13 @@ namespace Cibertec.WebApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IUnitOfWork>(option => new NorthwindUnitOfWork(Configuration.GetConnectionString("Northwind")));
-            services.AddMvc();
+            services.AddMvc().AddFluentValidation();
+            services.AddTransient<IValidator<Customer>, CustomerValidator>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
