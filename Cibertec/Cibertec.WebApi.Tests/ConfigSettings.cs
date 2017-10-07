b237b3ca84e7f5be63d454d3d603ae30d7385638
Julio Velarde <pxjuvela@cibertec.edu.pe>
2017-10-07 12:10:27 -0500

    Primer método de test.

---
+++ Cibertec/Cibertec.WebApi.Tests/ConfigSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cibertec.WebApi.Tests
+{
+    public static class ConfigSettings
+    {
+        public static string NorthwindConnectionString { get; } = "Server=.;Database=Northwind_Lite; Trusted_Connection=True;MultipleActiveResultSets=True";
+    }
+}
