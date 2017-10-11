a5853208a918fb685ee1b3c2dd1036474173334a
Cesar Velarde <juvega@gmail.com>
2017-10-11 17:26:06 -0500

    Configuracion básica de moq.

---
+++ Cibertec/Cibertec.Mocked/UnitOfWorkMocked.cs
@@ -0,0 +1,21 @@
+﻿using Cibertec.Repositories.Northwind;
+using System;
+using Cibertec.Models;
+using System.Collections.Generic;
+using Ploeh.AutoFixture;
+using System.Linq;
+using Moq;
+using Cibertec.UnitOfWork;
+
+namespace Cibertec.Mocked
+{
+    public class UnitOfWorkMocked
+    {
+        private List<Customer> _customerList;
+        public UnitOfWorkMocked()
+        {
+            var mocked = new Mock<IUnitOfWork>();
+
+        }
+    }
+}
