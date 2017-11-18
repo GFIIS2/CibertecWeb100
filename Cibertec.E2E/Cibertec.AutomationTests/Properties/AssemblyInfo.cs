5b431969cae7f8dcaba296fcdd596cdb9bfaa730
Julio Velarde <cevegaju@hotmail.com>
2017-11-18 03:37:16 -0500

    Configuración completa.

---
+++ Cibertec.E2E/Cibertec.AutomationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cibertec.AutomationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cibertec.AutomationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9e07726b-98aa-4644-ae71-e8573f08eb3f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Cibertec.E2E/Cibertec.AutomationTests/SimpleTest.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace Cibertec.AutomationTests
+{
+    public class SimpleTest
+    {
+        [Fact]
+        public void Navigate()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.Close();
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
